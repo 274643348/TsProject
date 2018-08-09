afd07f75f3333aa70356f007f704a6c36fe40100
XiaoYan <liujingyan@congmingpai.com>
2018-08-09 11:03:05 +0800

    对边写了点代码,验证ts智能提示;
    使用 TypeScript 脚本(参考路径):
    http://docs.cocos.com/creator/manual/zh/scripting/typescript.html?h=%E9%A1%B9%E7%9B%AE%E9%85%8D%E7%BD%AE

--- assets/Script/Helloworld.ts
+++ assets/Script/Helloworld.ts
@@ -6,11 +6,20 @@ export default class Helloworld extends cc.Component {
     @property(cc.Label)
     label: cc.Label = null;
 
+    @property([cc.Node])
+    cocoNode: cc.Node[]=[];
+
     @property
     text: string = 'hello';
 
     start () {
         // init logic
         this.label.string = this.text;
+
+        for (let index = 0; index < this.cocoNode.length; index++) {
+            const element = this.cocoNode[index];
+            element.runAction(cc.repeatForever(cc.sequence(cc.moveBy(0.5,cc.v2(50,50)),cc.moveBy(0.5,cc.v2(-50,-50)))));
+        }
+        
     }
 }
